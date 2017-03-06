50e44b14cf25d338ee51fa9bac7a9360ac65c5ce
Diggg <wgzhn@163.com>
2017-03-06 01:05:21 -0800

    20170306

--- searchengine.py
+++ searchengine.py
@@ -4,7 +4,7 @@ import traceback
 from bs4 import *
 import sqlite3 as sqlite
 import re
-ignoreword=set(['the','of','to','and','a','in','is','it'])
+ignorewords=set(['the','of','to','and','a','in','is','it'])
 
 class crawler:
     def __init__(self,dbname):
@@ -18,10 +18,36 @@ class crawler:
         self.con.commit()
 
     def getentryid(self,table,field,value,createnew=True):
-        return None
+        cur=self.con.execute(
+            "select rowid from %s where %s='%s'" %(table,field,value)
+        )
+        res=cur.fetchone()
+        if res==None:
+            cur=self.con.execute(
+                "insert into %s (%s) values ('%s')" % (table,field,value)
+            )
+            return cur.lastrowid
+        else:
+            return res[0]
 
     def addtoindex(self,url,soup):
+        if self.isindexed(url): return
         print('Indexing %s' % url)
+
+        #获取每个单词
+        text=self.gettextonly(soup)
+        words=self.separatewords(text)
+
+        #得到URL的id
+        urlid=self.getentryid('urllist','url',url)
+
+        #将每个单词与url关联
+        for i in range(len(words)):
+            word=words[i]
+            if word in ignorewords: continue
+            wordid=self.getentryid('wordlist','word',word)
+            self.con.execute("insert into wordlocation(urlid,wordid,location) values(%d,%d,%d)" %(urlid,wordid,i))
+
 
     def gettextonly(self,soup):
         v=soup.string
@@ -40,7 +66,15 @@ class crawler:
         return [s.lower() for s in splitter.split(text) if s!='']
 
     def isindexed(self,url):
-        return None
+        u=self.con.execute(
+            "select rowid from urllist where url='%s'" % url
+        ).fetchone()
+        if u!=None:
+            #检查是否已经被建立过索引
+            v=self.con.execute(
+                "select * from wordlocation where urlid=%d" % u[0]
+            ).fetchone()
+        return False
 
     def addlinkref(self,urlFrom,urlTo,linkText):
         pass
@@ -76,7 +110,7 @@ class crawler:
                     traceback.print_exc()
                     print("Could not open %s" % page)
                     continue
-                soup=BeautifulSoup(c,"lxml")
+                soup=BeautifulSoup(c,"html.parser")
                 self.addtoindex(page,soup)
 
                 links=soup('a')
@@ -95,8 +129,8 @@ class crawler:
 
 
 
-
-pagelist=['http://www.ihref.com/read-16514.html']
-crawler=crawler('searchindex.db')
-crawler.createindextables()
-#crawler.crawl(pagelist)
+if "__name__" =="__main__":
+    pagelist=['http://www.itpub.net/forum-2-1.html']
+    crawler=crawler('searchindex.db')
+    crawler.createindextables()
+    crawler.crawl(pagelist)
